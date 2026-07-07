0b23378f7ec32060988fe6eaca4093d25fe6e0dd
dout231 <211599672+dout231q1@users.noreply.github.com>
2026-07-06 23:48:54 -0300

    refactor(user): change ErrorResponse status field from HttpStatus to int

--- src/main/java/com/example/shop/infra/ErrorResponse.java
+++ src/main/java/com/example/shop/infra/ErrorResponse.java
@@ -3,13 +3,12 @@ package com.example.shop.infra;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.http.HttpStatus;
 
 
 @AllArgsConstructor
 @Getter
 @Setter
 public class ErrorResponse {
-    private HttpStatus status;
+    private Integer status;
     private String message;
 }
