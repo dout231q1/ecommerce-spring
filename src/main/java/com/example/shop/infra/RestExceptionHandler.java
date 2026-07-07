3b4673ab5ab17df41b6e4578255b6c2e96c63e5f
dout231 <211599672+dout231q1@users.noreply.github.com>
2026-07-06 23:58:06 -0300

    refactor(infra): update RestExceptionHandler to use int status from ErrorResponse

--- src/main/java/com/example/shop/infra/RestExceptionHandler.java
+++ src/main/java/com/example/shop/infra/RestExceptionHandler.java
@@ -16,7 +16,7 @@ public class RestExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     private ResponseEntity<ErrorResponse> resourceNotFoundHandler(ResourceNotFoundException exception) {
-        ErrorResponse error = new ErrorResponse(HttpStatus.NOT_FOUND,exception.getMessage());
+        ErrorResponse error = new ErrorResponse(HttpStatus.NOT_FOUND.value(),exception.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 
@@ -27,19 +27,19 @@ public class RestExceptionHandler {
                 .map(e -> e.getField() + ": " + e.getDefaultMessage())
                 .sorted()
                 .collect(Collectors.joining(" | "));
-        ErrorResponse error = new ErrorResponse(HttpStatus.BAD_REQUEST, message);
+        ErrorResponse error = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), message);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
     @ExceptionHandler(NoResourceFoundException.class)
     private ResponseEntity<ErrorResponse> notValidURLhandler(NoResourceFoundException exception){
-        ErrorResponse error = new ErrorResponse(HttpStatus.BAD_REQUEST, "Invalid URL '" + exception.getResourcePath() + "'. Please check the endpoint." );
+        ErrorResponse error = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "Invalid URL '" + exception.getResourcePath() + "'. Please check the endpoint." );
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     private ResponseEntity<ErrorResponse> methodArgumentTypeMismatchHandler(MethodArgumentTypeMismatchException exception) {
-        ErrorResponse error = new ErrorResponse(HttpStatus.BAD_REQUEST,  "'" + exception.getValue() + "' for parameter '" +  exception.getName() + "'. Excepted a valid number.'");
+        ErrorResponse error = new ErrorResponse(HttpStatus.BAD_REQUEST.value(),  "'" + exception.getValue() + "' for parameter '" +  exception.getName() + "'. Excepted a valid number.'");
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
