b110ecbf458bca286e6c332814752aa7a666723f
dout231 <211599672+dout231q1@users.noreply.github.com>
2026-07-13 19:41:55 -0300

    refactor(exception) restructure validation error response format

--- src/main/java/com/example/shop/infra/ErrorResponse.java
+++ src/main/java/com/example/shop/infra/ErrorResponse.java
@@ -1,14 +1,40 @@
 package com.example.shop.infra;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+import java.util.Map;
 
 
 @AllArgsConstructor
 @Getter
 @Setter
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({"timestamp", "status", "message", "fields"})
 public class ErrorResponse {
-    private Integer status;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime timestamp;
+    private String status;
     private String message;
+    private Map<String, String> fields;
+
+    public ErrorResponse(HttpStatus status, String message){
+        this.timestamp = LocalDateTime.now();
+        this.status = status.value() + " " + status.name();
+        this.message = message;
+    }
+
+    public ErrorResponse(HttpStatus status, String message, Map<String, String> fields) {
+        this.timestamp = LocalDateTime.now();
+        this.status = status.value() + " " + status.name();
+        this.message = message;
+        this.fields = fields;
+    }
 }
--- src/main/java/com/example/shop/infra/RestExceptionHandler.java
+++ src/main/java/com/example/shop/infra/RestExceptionHandler.java
@@ -3,43 +3,50 @@ package com.example.shop.infra;
 import com.example.shop.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
-import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class RestExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     private ResponseEntity<ErrorResponse> resourceNotFoundHandler(ResourceNotFoundException exception) {
-        ErrorResponse error = new ErrorResponse(HttpStatus.NOT_FOUND.value(),exception.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        ErrorResponse error = new ErrorResponse(status,exception.getMessage());
+        return ResponseEntity.status(status).body(error);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     private ResponseEntity<ErrorResponse> methodArgumentNotValidHandler(MethodArgumentNotValidException exception) {
-        String message = exception.getBindingResult().getFieldErrors()
-                .stream()
-                .map(e -> e.getField() + ": " + e.getDefaultMessage())
-                .sorted()
-                .collect(Collectors.joining(" | "));
-        ErrorResponse error = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), message);
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        Map<String, String> errors = new HashMap<>();
+
+        for(FieldError fieldError : exception.getBindingResult().getFieldErrors()){
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        ErrorResponse response = new ErrorResponse(status, "Validation failed for the submitted fields", errors);
+        return ResponseEntity.status(status).body(response);
     }
 
     @ExceptionHandler(NoResourceFoundException.class)
     private ResponseEntity<ErrorResponse> notValidURLhandler(NoResourceFoundException exception){
-        ErrorResponse error = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "Invalid URL '" + exception.getResourcePath() + "'. Please check the endpoint." );
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        ErrorResponse error = new ErrorResponse(status, "Invalid URL '" + exception.getResourcePath() + "'. Please check the endpoint." );
+        return ResponseEntity.status(status).body(error);
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     private ResponseEntity<ErrorResponse> methodArgumentTypeMismatchHandler(MethodArgumentTypeMismatchException exception) {
-        ErrorResponse error = new ErrorResponse(HttpStatus.BAD_REQUEST.value(),  "'" + exception.getValue() + "' for parameter '" +  exception.getName() + "'. Excepted a valid number.'");
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        ErrorResponse error = new ErrorResponse(status,  "'" + exception.getValue() + "' for parameter '" +  exception.getName() + "'. Excepted a valid number.'");
+        return ResponseEntity.status(status).body(error);
     }
 }
